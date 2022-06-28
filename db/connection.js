f652c55a5d0357cf5ecc7d822832c972bffd4c1c
PhongDo <dovanphong96@gmail.com>
2022-06-28 22:15:19 +0700

    remove reduntdant file

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 // Include mongoose module
 const mongoose = require('mongoose');
-const { DB_NAME, DB_USER, DB_PASS, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT } = process.env;
 // Creating new Object of mongoose
 //mongodb+srv://admin:<password>@cluster0.znbih.mongodb.net/covid?retryWrites=true&w=majority
-const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:27017/${DB_NAME}?authSource=admin`;
+const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 const connection = mongoose.createConnection(url);
 // postgres://db:5432
 
